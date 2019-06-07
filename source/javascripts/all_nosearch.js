082406c6b485f612455d4617c7dd12f1ec2741c6
deploy.sh <nils.dvl+git@gmail.com>
2019-06-07 14:23:26 +0000

    fix cross domain error with postMessage api

--- source/javascripts/all_nosearch.js
+++ source/javascripts/all_nosearch.js
@@ -4,32 +4,15 @@
 
 var lastEventAnchor = "";
 
-const DOC_ANCHOR_UPDATED = "docAnchorUpdated"; // anchor updated after scroll/click in the iframe
-const DOC_ANCHOR_REQUESTED = "docAchorRequested"; // parent window is requesting a specific anchor, force iframe navigating to it
-
 const sanitizeLink = function(a){
   if((typeof a.href == "string") && !/^#|^\?|(firekast\.io\/docs)|(docs\.firekast\.io)|(localhost:4567)/.test(a.href) ){
     a.target = "_blank"
-  } else {
-    a.target = ""
   }
 }
 
 const deepDocLink = function(url){
   return url.search + url.hash
 }
-
-const fkio_updateParentWindow = function(){
-  const e = new CustomEvent(DOC_ANCHOR_UPDATED, {detail: deepDocLink(window.location)});
-  window.parent.document.dispatchEvent(e);
-};
-
-const onNavRequested = function(e){
-  if(!e.detail){ return }
-  loc = e.detail;
-  lastEventAnchor = e.detail;
-  window.location.assign(e.detail);
-};
 
 $(function() {
   loadToc($('#toc'), '.toc-link', '.toc-list-h2', 10);
@@ -39,8 +22,19 @@ $(function() {
     window.refreshToc();
   });
   $('.content a').each(function(i,a){sanitizeLink(a)});
-  window.document.addEventListener(DOC_ANCHOR_REQUESTED, onNavRequested);
 });
+
+const fkio_updateParentWindow = function(){
+  window.parent.postMessage('docAnchorUpdated'+'::'+deepDocLink(window.location),"*")
+};
+
+window.onmessage = function(e){
+  var re = /^docAnchorRequested::/
+  if(re.test(e.data)){
+    lastEventAnchor = e.data.replace(re,'');
+    window.location.assign(e.detail);  
+  }
+}
 
 window.onpopstate = function() {
   activateLanguage(getLanguageFromQueryString());
